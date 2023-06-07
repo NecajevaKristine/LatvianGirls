e80f6d40275b4206bef4a97df7e76fcd523bb7b6
Rita <ritucis@inbox.lv>
2023-06-07 21:03:26 +0100

    Email is not yet :)

--- src/main/java/com/latviangirls/SendEmail/EmailSendController.java
+++ src/main/java/com/latviangirls/SendEmail/EmailSendController.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @SpringBootApplication
 public class EmailSendController {
@@ -21,4 +24,5 @@ public class EmailSendController {
                 "This is email subject",
                 "This is email body");
     }
+
 }
